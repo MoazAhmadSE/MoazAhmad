5658780d765d3e2dd1a7dbfd51defdc8976a5b84
MOAZ AHMAD <126986397+MoazAhmadSE@users.noreply.github.com>
2025-07-13 13:41:29 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,7 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
